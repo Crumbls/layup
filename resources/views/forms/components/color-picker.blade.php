3241eb1b55d3742b73e66b31915c6bf84b113dae
Chase Miller <chase@crumbls.com>
2026-03-29 17:49:51 -0600

    Fix contrast color call to use field instance instead of Livewire component

--- resources/views/forms/components/color-picker.blade.php
+++ resources/views/forms/components/color-picker.blade.php
@@ -3,6 +3,10 @@
     $allowCustom = $getAllowCustom();
     $fieldId = $getId();
     $statePath = $getStatePath();
+    $contrastColors = [];
+    foreach ($swatches as $name => $hex) {
+        $contrastColors[$name] = $field->getContrastColor($hex);
+    }
 @endphp
 
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
@@ -64,7 +68,7 @@
                     class="absolute inset-0 flex items-center justify-center"
                 >
                     <svg class="w-4 h-4 drop-shadow-sm" viewBox="0 0 20 20" fill="currentColor"
-                         style="color: {{ $this->getContrastColor($hex) }}">
+                         style="color: {{ $contrastColors[$name] }}">
                         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
                     </svg>
                 </span>
